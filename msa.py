63a2fc99bb02d11a7a8e5d71eb369b34c7f05533
Nizar Mahmoud <nizarmah@hotmail.com>
2019-04-15 11:52:48 +0300

    Initial Commit - Parallelized P Matrix

---
+++ msa.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from Bio.Align.Applications import ClustalOmegaCommandline
+
+def main(argv):
+	# output file is the first argument
+	out_file = argv[0]
+	# all other arguments are input / fasta files
+	in_files = " ".join(argv[1:])
+
+	# save all fastas in one file using `cat`
+	cin_file = ".cin"
+	os.system("cat " + in_files + " > " + cin_file)
+
+	# get clustal's command
+	clustalomega_cline = ClustalOmegaCommandline(infile=cin_file, outfile=out_file,
+							verbose=True, auto=True)
+	# run clustal's command
+	os.system("./" + str(clustalomega_cline))
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
